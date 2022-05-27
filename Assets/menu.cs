425f58125bef0407c68533d14f3503c4e17851c4
BrownieBearApocolypse <maddyellicott195@gmail.com>
2022-05-27 16:30:06 +1000

    create git

---
+++ Assets/menu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Canvas Howto;
+
+        private void Awake()
+    {
+        Howto.enabled = false;
+    }
+	public void HowtoON()
+{
+        Howto.enabled = true;
+        MainCanvas.enabled = false;
+}
+
+    public void ReturnOn()
+    {
+        Howto.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+
---
+++ Assets/teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+
+    public GameObject teleport2;
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            player.transform.position = new Vector2(-7.49f, -1.21f);
+    }
+}
