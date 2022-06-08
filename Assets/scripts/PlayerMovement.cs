e012696a7f2b075b86e79f94422a53fca9826bdb
BrownieBearApocolypse <maddyellicott195@gmail.com>
2022-06-08 16:09:48 +1000

    fixed last of things needed forfor focus on script work
    fixed playermovent cause script from og wasnt working and made simple version of oncollsiondestroy

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jump = 25f;
+    float moveVelocity;
+    public Rigidbody2D rb2d;
+    bool isGrounded = true;
+    float threshold = -50f;
+
+    void Update()
+    {
+        if (transform.position.y < threshold)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+        //jumping & grounded?
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            isGrounded = false;
+        }
+
+        
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.CompareTag("Platform"))
+       {
+            isGrounded = true;
+       }
+       
+    }
+}
