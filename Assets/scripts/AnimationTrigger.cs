cc9f8b39c16e98d24e196ca58b730d2068b9cf85
BrownieBearApocolypse <maddyellicott195@gmail.com>
2022-06-09 16:10:46 +1000

    aahhhhhh i need to get out of this namiong habit
    continued with editing environment and codes already made, did animation trigger code and updated it to work multiple times

--- Assets/scripts/AnimationTrigger.cs
+++ Assets/scripts/AnimationTrigger.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class AnimationTrigger : MonoBehaviour
 {
+    public GameObject Button;
+    public bool Triggered;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Triggered = false;
     }
 
     // Update is called once per frame
-    void Update()
+
+
+    public void OnTriggerEnter2D (Collider2D other)
     {
-        
+        if (Triggered == false && other.gameObject.tag == "Player")
+        {
+            Triggered = true;
+            Button.GetComponent<Animator>().SetTrigger("ButtonPress");
+            Debug.Log("it works");
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        Triggered = false;
     }
 }
